e17025e79cddb32e4b45eb8e77eb0541112a932d
Eros Rolfe <erosrolf@pr-m5.kzn.21-school.ru>
2024-03-19 10:16:14 +0300

    add rotate strategy

--- src/model/obj_modifier_stategy.cc
+++ src/model/obj_modifier_stategy.cc
@@ -1,15 +1,69 @@
 #include "obj_modifier_stategy.h"
 
+#include <cmath>
+
 #include "obj.h"
 
 namespace s21 {
 
-void RotateStrategy::Modify(Obj& obj) {
-  for (auto& vertex : obj.vertexes) {
+// Rotate Strategy -------
+
+void RotateStrategy::modify(Obj& obj) {
+  if (angle_ != 0.0) {
+    switch (type_) {
+      case Type::X: {
+        rotate(obj, angle_, &RotateStrategy::rotateX);
+        return;
+      }
+      case Type::Y: {
+        rotate(obj, angle_, &RotateStrategy::rotateY);
+        return;
+      }
+      case Type::Z: {
+        rotate(obj, angle_, &RotateStrategy::rotateZ);
+        return;
+      }
+    }
   }
 }
 
-void ScalingStrategy::Modify(Obj& obj) {
+void RotateStrategy::rotate(Obj& obj, double angle, RotateFunction rotateFunc) {
+  double rad = angle * M_PI / 180.0;
+  double cos_angle = cos(rad);
+  double sin_angle = sin(rad);
+
+  for (auto& vertex : obj.vertexes) {
+    (this->*rotateFunc)(vertex, sin_angle, cos_angle);
+  }
+}
+
+void RotateStrategy::rotateX(Vertex_3d& vertex, double sin_angle,
+                             double cos_angle) {
+  double new_y = vertex.y * cos_angle - vertex.z * sin_angle;
+  double new_z = vertex.y * sin_angle + vertex.z * cos_angle;
+  vertex.y = new_y;
+  vertex.z = new_z;
+}
+
+void RotateStrategy::rotateY(Vertex_3d& vertex, double sin_angle,
+                             double cos_angle) {
+  double new_x = vertex.x * cos_angle - vertex.z * sin_angle;
+  double new_z = vertex.x * sin_angle + vertex.z * cos_angle;
+  vertex.x = new_x;
+  vertex.z = new_z;
+}
+
+void RotateStrategy::rotateZ(Vertex_3d& vertex, double sin_angle,
+                             double cos_angle) {
+  double new_x = vertex.x * cos_angle - vertex.y * sin_angle;
+  double new_y = vertex.x * sin_angle + vertex.y * cos_angle;
+  vertex.x = new_x;
+  vertex.y = new_y;
+}
+
+// Scaling Strategy -------
+
+void ScalingStrategy::modify(Obj& obj) {
   for (auto& vertex : obj.vertexes) {
     vertex.x *= s_;
     vertex.y *= s_;
@@ -17,7 +71,9 @@ void ScalingStrategy::Modify(Obj& obj) {
   }
 }
 
-void TranslationStrategy::Modify(Obj& obj) {
+// Translation Strategy -------
+
+void TranslationStrategy::modify(Obj& obj) {
   for (auto& vertex : obj.vertexes) {
     vertex.x += x_;
     vertex.y += y_;
--- src/model/obj_modifier_stategy.h
+++ src/model/obj_modifier_stategy.h
@@ -1,6 +1,8 @@
 #ifndef SRC_3DVIEWER_MODEL_OBJ_MODIFIER_STRATEGY_H_
 #define SRC_3DVIEWER_MODEL_OBJ_MODIFIER_STRATEGY_H_
 
+#include "obj.h"
+
 namespace s21 {
 
 class Obj;
@@ -8,34 +10,44 @@ class Obj;
 class ObjModifier {
  public:
   virtual ~ObjModifier() = default;
-  virtual void Modify(Obj& obj) = 0;
+  virtual void modify(Obj& obj) = 0;
 };
 
 class CentringStrategy : public ObjModifier {
  public:
-  void Modify(Obj& obj) override;
+  void modify(Obj& obj) override;
   // TODO Написать алгоритм центрирования в .cc
 };
 
 class RotateStrategy : public ObjModifier {
  public:
-  RotateStrategy(double x, double y, double z)
-      : angle_x_(x), angle_y_(y), angle_z_(z) {}
+  enum class Type {
+    X = 0,
+    Y,
+    Z,
+  };
 
-  void Modify(Obj& obj) override;
+  RotateStrategy(Type type, double angle) : type_(type), angle_(angle) {}
+
+  void modify(Obj& obj) override;
 
  private:
-  void rotateX(Obj& obj, double angle_x_);
-  void rotateY(Obj& obj, double angle_y_);
-  void rotateZ(Obj& obj, double angle_z_);
-  double angle_x_, angle_y_, angle_z_;
+  using RotateFunction = void (RotateStrategy::*)(Vertex_3d&, double, double);
+
+  void rotate(Obj& obj, double angle, RotateFunction rotateFunc);
+  void rotateX(Vertex_3d& vertex, double sin_angle, double cos_angle);
+  void rotateY(Vertex_3d& vertex, double sin_angle, double cos_angle);
+  void rotateZ(Vertex_3d& vertex, double sin_angle, double cos_angle);
+
+  Type type_;
+  double angle_;
 };
 
 class ScalingStrategy : public ObjModifier {
  public:
   ScalingStrategy(double s) : s_(s) {}
 
-  void Modify(Obj& obj) override;
+  void modify(Obj& obj) override;
 
  private:
   double s_;
@@ -45,7 +57,7 @@ class TranslationStrategy : public ObjModifier {
  public:
   TranslationStrategy(double x, double y, double z);
 
-  void Modify(Obj& obj) override;
+  void modify(Obj& obj) override;
 
  private:
   double x_, y_, z_;
