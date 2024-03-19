0a16d2eb1eebeb4b005141e157796118dd113b97
Eros Rolfe <erosrolf@pr-m5.kzn.21-school.ru>
2024-03-19 13:44:45 +0300

    fixed Makefile

--- src/core/obj_modifier_strategy/translation_strategy.cc
+++ src/core/obj_modifier_strategy/translation_strategy.cc
@@ -6,6 +6,9 @@
 
 namespace s21 {
 
+TranslationStrategy::TranslationStrategy(double x, double y, double z)
+    : x_(x), y_(y), z_(z) {}
+
 void TranslationStrategy::modify(Obj& obj) {
   for (auto& vertex : obj.vertexes) {
     vertex.x += x_;
