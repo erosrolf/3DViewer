55a7681adf6231418782de9c6823121260e14316
Eros Rolfe <erosrolf@pr-m5.kzn.21-school.ru>
2024-03-19 14:59:03 +0300

    fixed test for translate strategy

--- src/tests/obj_modifier_test.cc
+++ src/tests/obj_modifier_test.cc
@@ -1,20 +1,25 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
+
 #include "../core/obj.h"
 #include "../core/obj_modifier_strategy/obj_modifier_strategy.h"
 
 TEST(obj_translation, x_translation) {
   s21::Obj obj("obj_resurces/cube.obj");
-
-  for (auto it : obj.vertexes) {
-    std::cout << it.x << ' ' << it.y << ' ' << it.z << '\n';
-  }
-
   s21::TranslationStrategy moveLeft(-1, 0, 0);
-
   obj.modify(&moveLeft);
 
-  for (auto it : obj.vertexes) {
-    std::cout << it.x << ' ' << it.y << ' ' << it.z << '\n';
+  std::vector<s21::Vertex_3d> ref = {{0.0, 1.0, -1.0},  {0.0, -1.0, -1.0},
+                                     {0.0, 1.0, 1.0},   {0.0, -1.0, 1.0},
+                                     {-2.0, 1.0, -1.0}, {-2.0, -1.0, -1.0},
+                                     {-2.0, 1.0, 1.0},  {-2.0, -1.0, 1.0}};
+
+  auto ref_vertex = ref.begin();
+  for (auto vertex : obj.vertexes) {
+    ASSERT_EQ(vertex.x, ref_vertex->x);
+    ASSERT_EQ(vertex.y, ref_vertex->y);
+    ASSERT_EQ(vertex.z, ref_vertex->z);
+    ++ref_vertex;
   }
 }
